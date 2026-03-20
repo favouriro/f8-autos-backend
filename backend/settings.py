d833e258efd14410ee8d7511bd51e696c6232a70
Favouriwegbu <favour4iwegbu@gmail.com>
2026-03-20 13:03:31 +0000

    Add FRONTEND_URL to CORS allowed origins

--- backend/settings.py
+++ backend/settings.py
@@ -143,6 +143,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://localhost:5173',
+    os.getenv('FRONTEND_URL', ''),
 ]
 
 # REST Framework
