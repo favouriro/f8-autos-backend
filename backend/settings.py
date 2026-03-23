3ca48df33ab65ab735e4a3318e99458d25207f7b
Favouriwegbu <favour4iwegbu@gmail.com>
2026-03-23 16:27:34 +0000

    Fix CORS headers and credentials for production

--- backend/settings.py
+++ backend/settings.py
@@ -146,6 +146,20 @@ CORS_ALLOWED_ORIGINS = [
     os.getenv('FRONTEND_URL', ''),
 ]
 
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
 # REST Framework
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
